39a32ebbf1d02bf75ffd765480b2a7a5dded0079
Sagar Limaye <sagarl3232@hotmail.com>
2019-11-19 14:14:47 -0500

    getter and setters for channel

--- src/data/Channel.java
+++ src/data/Channel.java
@@ -1,14 +1,50 @@
 package data;
 
 public class Channel {
-	private String channel_name = "";
-
-	public String getChannel_name() {
-		return channel_name;
+	private String name, band, chargeType, transmissionType;
+	private float videoFreq, audioFreq, charge;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBand() {
+		return band;
+	}
+	public void setBand(String band) {
+		this.band = band;
+	}
+	public String getChargeType() {
+		return chargeType;
+	}
+	public void setChargeType(String chargeType) {
+		this.chargeType = chargeType;
+	}
+	public String getTransmissionType() {
+		return transmissionType;
+	}
+	public void setTransmissionType(String transmissionType) {
+		this.transmissionType = transmissionType;
+	}
+	public float getVideoFreq() {
+		return videoFreq;
+	}
+	public void setVideoFreq(float videoFreq) {
+		this.videoFreq = videoFreq;
+	}
+	public float getAudioFreq() {
+		return audioFreq;
+	}
+	public void setAudioFreq(float audioFreq) {
+		this.audioFreq = audioFreq;
+	}
+	public float getCharge() {
+		return charge;
+	}
+	public void setCharge(float charge) {
+		this.charge = charge;
 	}
 
-	public void setChannel_name(String channel_name) {
-		this.channel_name = channel_name;
-	}
 	
 }
